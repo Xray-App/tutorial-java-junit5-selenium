0520f6c2c1b1fc942944260781eb9208a38f4961
CMCunha <cristiano.cunha@gmail.com>
2021-09-21 14:21:01 +0100

    Adding missing files.

---
+++ src/test/java/com/RepositoryParser.java
@@ -0,0 +1,31 @@
+package com.xpandit.xray.tutorials;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.nio.file.Path;
+
+public class RepositoryParser
+{
+
+	private FileInputStream stream;
+	private String RepositoryFile;
+	private Properties propertyFile = new Properties();
+
+	public RepositoryParser(String fileName)
+	{
+        try {
+            this.RepositoryFile = fileName;
+		    stream = new FileInputStream(RepositoryFile);
+		    propertyFile.load(stream);    
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+		
+	}
+
+    public String getBy(String locatorName){
+        return propertyFile.getProperty(locatorName);
+    }
+
+}
